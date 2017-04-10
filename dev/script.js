9664d6ca6378791b6cc84a361ed970519bc77d47
Kaushal Kumar <researcherkumar@gmail.com>
2017-04-10 02:13:32 -0700

    Update script.js

--- dev/script.js
+++ dev/script.js
@@ -3,13 +3,14 @@ if(v.addEventListener)
 	v.addEventListener("click", func, false);
 else if(v.attachEvent)
 	v.attachEvent('conclick', func);
-function func(){
-	var ta = document.getElementById('divide');
-	var x = new XMLHttpRequest;
-	x.open("GET", "test.html");
-	x.onreadystatechange =function(){
+
+var ta = document.getElementById('divide');
+var x = new XMLHttpRequest;
+x.open("GET", "test.html");
+x.onreadystatechange =function(){
+	function func(){
 		x.responseType = "document";
 		ta.innerHTML = x.responseText;
 	}
 }
-	x.send();
+x.send();
